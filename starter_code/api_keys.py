506c964250e05d6a0cd9542384397428dcfb70a6
Michael Ejike <mikesoccer12@gmail.com>
2020-10-27 02:01:18 -0400

    almost done

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR kEY HERE"
+weather_api_key = "YOUR KEY HERE!"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCo7-B9p9hqoYr-GsHJ-Nsg0HXPeX9uDrg"
